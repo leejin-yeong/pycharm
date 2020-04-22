880c13b1a27dc94917841850354d0f3b6ac9be04
JinYoung Lee <cookie178@naver.com>
2020-04-22 22:32:00 +0900

    Updates on Local Master Branch

--- main.py
+++ main.py
@@ -17,3 +17,7 @@ print("Hello World 5")
 
 #updates in main branch of GitHub
 print("Hello World Update on Github branch")
+
+
+#adding new item in local master branch
+print("Hello World Updates on Local Master Branch ")
